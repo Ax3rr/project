1228199fb28f4a8c3efc53ecd17c56f80447047f
Nick <simonovv1922@yandex.ru>
2021-02-11 15:46:21 +0300

    2

--- lesson4/main.js
+++ lesson4/main.js
@@ -1,34 +1,20 @@
 "use strict"
 
-function Person(name,lastname,year) {
-    this.name = name;
-    this.lastname = lastname;
-    this.age = 2021-year;
+
+let num = prompt("Введите число");
+
+let endNum = {
+    десятки:Math.floor (num/10%10),
+    единицы:Math.floor (num%10),
+    сотни:Math.floor (num/100%10)
+
+};
+
+if (num<=999) {
+    console.log(endNum);
+
 }
 
-let people = [
-    new Person('John','Doe',1985),
-    new Person('Jane','Doe',1993),
-    new Person('Sam','Doe',1974),
-    new Person('Kate','Doe',2005),
-]
-
-function getAge(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++){
-    sum += arr[i][`age`];
-}
-return sum / arr.length;
-}
-
-console.log (getAge(people));
-
-let n = 245;
-
-let num = [
-    ed = n%10,
-    des=n%,
-    console.log(ed),
-    console.log(des)
-
-]
+else {
+    console.log("ERROR")
+};
